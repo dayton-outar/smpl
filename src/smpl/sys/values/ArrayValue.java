f98c750b5175521cd48f631d200b988bcf2d94be
Dayton Outar <dayton.outar@gmail.com>
2021-04-18 15:08:53 -0500

    Completed implementation for more functions

--- src/smpl/sys/values/ArrayValue.java
+++ src/smpl/sys/values/ArrayValue.java
@@ -206,31 +206,147 @@ public class ArrayValue implements IValue {
     @Override
     public IValue pow(IValue val) throws Exception {
         if (val.isArray()) {
-            return this.mul( (ArrayValue) val );
+            return this.pow( (ArrayValue) val );
         } else {
-            return val.isLong() ? val.mul( (ArrayValue) this) : val.mul( (ArrayValue) this );
+            return val.isLong() ? this.pow( (LongValue) val) : this.pow( (DoubleValue) val );
         }
+    }
+
+    public IValue pow(LongValue val) throws Exception {
+        Vector<IValue> lvs = new Vector<IValue>();
+
+        // The intention is to perform [1, 3, 2]**2 => [1, 9, 4]
+        for (IValue ival : _val) {
+            lvs.add( ival.pow( val ) );
+        }
+        
+        return new ArrayValue( lvs );
+    }
+
+    public IValue pow(DoubleValue val) throws Exception {
+        Vector<IValue> lvs = new Vector<IValue>();
+
+        // The intention is to perform [1, 3, 2]**2 => [1, 9, 4]
+        for (IValue ival : _val) {
+            lvs.add( ival.pow( val ) );
+        }
+        
+        return new ArrayValue( lvs );
+    }
+
+    public IValue pow(ArrayValue val) throws Exception {
+        Vector<IValue> lvs = new Vector<IValue>();
+        Vector<IValue> vals = val.arrayValues();
+
+        // The intention is to perform [2, 3] + [1, 3, 2] => [3, 6, 2]
+        int max = _val.size() >= vals.size() ? _val.size() : vals.size();        
+        for (int x = 0; x < max; x++) {
+            if (x < _val.size() & x < vals.size()) {
+                lvs.add( _val.get(x).pow( vals.get(x) ) );
+            } else {
+                if ( x >= _val.size() ) {
+                    IValue v = vals.get(x);
+                    lvs.add( v.isLong() ? (new LongValue(0l)).pow( v ) : (new DoubleValue(0.0)).pow( v ) );
+                } else {
+                    lvs.add( _val.get(x) );
+                }
+            }
+        }
+        
+        return new ArrayValue( lvs );
     }
 
     @Override
     public IValue ban(IValue val) throws Exception {
         if (val.isArray()) {
-            return this.mul( (ArrayValue) val );
+            return this.ban( (ArrayValue) val );
         } else {
-            return val.isLong() ? val.mul( (ArrayValue) this) : val.mul( (ArrayValue) this );
+            return val.isLong() ? val.ban( (ArrayValue) this) : val.ban( (ArrayValue) this );
         }
+    }
+
+    public IValue ban(ArrayValue val) throws Exception {
+        Vector<IValue> lvs = new Vector<IValue>();
+        Vector<IValue> vals = val.arrayValues();
+
+        // The intention is to perform [2, 3] + [1, 3, 2] => [3, 6, 2]
+        int max = _val.size() >= vals.size() ? _val.size() : vals.size();        
+        for (int x = 0; x < max; x++) {
+            if (x < _val.size() & x < vals.size()) {
+                lvs.add( _val.get(x).ban( vals.get(x) ) );
+            } else {
+                if ( x >= _val.size() ) {
+                    IValue v = vals.get(x);
+                    lvs.add( v.isLong() ? (new LongValue(0l)).ban( v ) : (new DoubleValue(0.0)).ban( v ) );
+                } else {
+                    lvs.add( _val.get(x) );
+                }
+            }
+        }
+        
+        return new ArrayValue( lvs );
     }
 
     @Override
     public IValue bor(IValue val) throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+        if (val.isArray()) {
+            return this.bor( (ArrayValue) val );
+        } else {
+            return val.isLong() ? val.bor( (ArrayValue) this) : val.bor( (ArrayValue) this );
+        }
+    }
+
+    public IValue bor(ArrayValue val) throws Exception {
+        Vector<IValue> lvs = new Vector<IValue>();
+        Vector<IValue> vals = val.arrayValues();
+
+        // The intention is to perform [2, 3] + [1, 3, 2] => [3, 6, 2]
+        int max = _val.size() >= vals.size() ? _val.size() : vals.size();        
+        for (int x = 0; x < max; x++) {
+            if (x < _val.size() & x < vals.size()) {
+                lvs.add( _val.get(x).bor( vals.get(x) ) );
+            } else {
+                if ( x >= _val.size() ) {
+                    IValue v = vals.get(x);
+                    lvs.add( v.isLong() ? (new LongValue(0l)).bor( v ) : (new DoubleValue(0.0)).bor( v ) );
+                } else {
+                    lvs.add( _val.get(x) );
+                }
+            }
+        }
+        
+        return new ArrayValue( lvs );
     }
 
     @Override
     public IValue bxr(IValue val) throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+        if (val.isArray()) {
+            return this.bxr( (ArrayValue) val );
+        } else {
+            return val.isLong() ? val.bxr( (ArrayValue) this) : val.bxr( (ArrayValue) this );
+        }
+    }
+
+    public IValue bxr(ArrayValue val) throws Exception {
+        Vector<IValue> lvs = new Vector<IValue>();
+        Vector<IValue> vals = val.arrayValues();
+
+        // The intention is to perform [2, 3] + [1, 3, 2] => [3, 6, 2]
+        int max = _val.size() >= vals.size() ? _val.size() : vals.size();        
+        for (int x = 0; x < max; x++) {
+            if (x < _val.size() & x < vals.size()) {
+                lvs.add( _val.get(x).bxr( vals.get(x) ) );
+            } else {
+                if ( x >= _val.size() ) {
+                    IValue v = vals.get(x);
+                    lvs.add( v.isLong() ? (new LongValue(0l)).bxr( v ) : (new DoubleValue(0.0)).bxr( v ) );
+                } else {
+                    lvs.add( _val.get(x) );
+                }
+            }
+        }
+        
+        return new ArrayValue( lvs );
     }
 
     @Override
